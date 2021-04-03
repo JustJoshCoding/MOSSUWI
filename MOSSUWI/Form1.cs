f0f391ad949ab3662038f05ab770d4f169782c91
Joshua Obrien <joshua.obrien@my.uwi.edu>
2021-04-03 17:37:27 -0400

    testing drag and drop

--- MOSSUWI/Form1.cs
+++ MOSSUWI/Form1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.Graph;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +15,81 @@ namespace MOSSUWI
 {
     public partial class Form1 : Form
     {
+        private bool validData;
+        private string lastFilename;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void pictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            Debug.WriteLine("OnDragEnter");
+            string filename;
+            validData = GetFilename(out filename, e);
+            if (validData)
+            {
+                if (lastFilename != filename)
+                {
+                    Thumbnail.Image = null;
+                    Thumbnail.Visible = false;
+                    lastFilename = filename;
+                    getImageThread = new Thread(new ThreadStart(LoadImage));
+                    getImageThread.Start();
+                }
+                else
+                {
+                    thumbnail.Visible = true;
+                }
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        protected bool GetFilename(out string filename, DragEventArgs e)
+        {
+            bool ret = false;
+            filename = String.Empty;
+
+            if ((e.AllowedEffect & DragDropEffects.Copy) == DragDropEffects.Copy)
+            {
+                Array data = ((IDataObject)e.Data).GetData("FileName") as Array;
+                if (data != null)
+                {
+                    if ((data.Length == 1) && (data.GetValue(0) is String))
+                    {
+                        filename = ((string[])data)[0];
+                        string ext = Path.GetExtension(filename).ToLower();
+                        if ((ext == ".zip") || (ext == ".dir"))
+                        {
+                            ret = true;
+                        }
+                    }
+                }
+            }
+            return ret;
+        }
+        public delegate void AssignImageDlgt();
+
+        protected void LoadImage()
+        {
+            nextImage = new Bitmap(lastFilename);
+            this.Invoke(new AssignImageDlgt(AssignImage));
+        }
+
+        protected void AssignImage()
+        {
+            thumbnail.Width = 100;
+            // 100 iWidth
+            // ---- = ------
+            // tHeight iHeight
+            thumbnail.Height = nextImage.Height * 100 / nextImage.Width;
+            SetThumbnailLocation(this.PointToClient(new Point(lastX, lastY)));
+            thumbnail.Image = nextImage;
+        }
     }
+
 }
